1bd280690cd80c963a45e2c961bade5b960ac386
JustADataConstruct <yarukatsaros@gmail.com>
2020-12-27 00:40:10 +0100

    Forgot some more docstrings.

--- cache_helper.py
+++ cache_helper.py
@@ -11,11 +11,25 @@ from output_helper import OutputHelper
 
 class CacheHelper():
     def __init__(self,output_helper,config):
+        """Handles loading and saving to and from the rsscache.json file and keeping the data up to date.
+
+        Args:
+            output_helper (OutputHelper object): An initialized OutputHelper instance. Used to print 
+            status messages.
+            config (object): A json object parsing the config.json file. 
+        """
         self.output = output_helper
         self.config = config
         self.verbose = config["verbose_mode"]
 
     def save_cache_file(self,feedname,feed_content):
+        """Tries to update the cached data of the indicated feed. If the cache file doesn't exist,
+        will create it.
+
+        Args:
+            feedname (string): Name of the feed to update
+            feed_content (object): Feed object to save.
+        """
         if os.path.isfile('rsscache.json'):
             if self.verbose:print("Cache file exists")
             with open('rsscache.json') as f:
@@ -30,6 +44,16 @@ class CacheHelper():
         f.close()
 
     def load_from_cache(self,feedname):
+        """Tries to load the cached data for the indicated feed from the cache file. Will handle errors if
+        the cache file is missing or the key can't be found.
+
+        Args:
+            feedname (string): Name of the feed to load.
+
+        Returns:
+            object: Json object with the feed data.
+            None: If the file can't be found or there's no data for this feed.
+        """
         try:
             with open('rsscache.json') as f:
                 if self.verbose:print("Loading from cache")
@@ -44,6 +68,11 @@ class CacheHelper():
 
 
     def remove_from_cache(self,feedname):
+        """Tries to remove a feed's data from the cache file. Used when the user unsuscribes from a feed.
+
+        Args:
+            feedname (string): Name of the feed.
+        """
         try:
             with open('rsscache.json') as f:
                 cache = json.loads(f.read())
@@ -55,6 +84,19 @@ class CacheHelper():
             self.output.write_error(e)
    
     def check_cache_valid(self,name,feed,last_check,to_console,force_refresh):
+        """Checks if has been enough time since the last time the server was called, tries to fetch
+        new data from it if so, and handles the different HTML codes it can receive. Will update feedinfo
+        and rsscache as needed to adjust to changes and updates. Will fail silently if it receives an
+        unknown code.
+
+        Args:
+            name (string): Name of the feed to check.
+            feed (object): Feed object to get data from.
+            last_check (datetime): Datetime object with the last time the feed was checked for updates.
+            to_console (bool): If True, will print status messages to terminal.
+            force_refresh (bool): If True, will download new data from the server and refresh cache even if
+            there are no changes.
+        """
         etag = feed["etag"]
         modified = feed["last-modified"]
         diff = datetime.now() - last_check
